9f7fdfd8260d4fd414713bbac08ca849700e0273
Akash Nair <nairakash128@gmail.com>
2026-02-25 02:05:58 +0530

    Final Polish - Premium KD Hospital Website

--- push_to_git.bat
+++ push_to_git.bat
@@ -1,6 +1,9 @@
-git rm -r --cached .
+@echo off
+git init
 git add .
 git commit -m "Final Polish - Premium KD Hospital Website"
-git remote add origin https://github.com/akashnair14/kdhospital.git
+git remote remove origin
+git remote add origin "https://github.com/akashnair14/kdhospital.git"
 git branch -M main
-git push -u origin main
+git push -u origin main -f
+echo DONE
